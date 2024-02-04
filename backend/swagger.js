1d90436dc70b6e4d67a2efe771932ca5d1320ad1
Innocent Twagirimana <itwagiri@andrew.cmu.edu>
2024-02-04 22:40:00 +0200

    add join community swagger doc

--- backend/swagger.js
+++ backend/swagger.js
@@ -2,9 +2,9 @@ export const swaggerOptions = {
     definition: {
         openapi: '3.0.0',
         info: {
-          title: 'Your API Title',
+          title: 'API for Emergency Social Network (ESN) Application',
           version: '1.0.0',
-          description: 'Your API description',
+          description: 'ESN is a social network that citizens can use during emergency situations.',
         },
         servers: [
           {
