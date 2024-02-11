30edc32c4b167d3e44a0ea7e742d41b8c7a63d5c
yuyunfrancis <francisberi@yahoo.com>
2024-02-11 14:43:29 +0200

    feat: landing page initialized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard.jsx';
 import Login from './pages/Login.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
+import LandingPage from './pages/LandingPage.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Navigate replace to="dashboard" />} />
+        <Route index element={<Navigate replace to="landingpage" />} />
+        <Route path="landingpage" element={<LandingPage />} />
+        {/* <Route index element={<Navigate replace to="dashboard" />} /> */}
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
