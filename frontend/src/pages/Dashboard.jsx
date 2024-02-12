13cb459d783acdb0aa65d6598ccf72387a780dae
kennyrogers330 <kennyrogers330@gmail.com>
2024-02-12 10:08:18 +0200

    completed the Members Directory component and had messages window partially

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,21 @@
-import Button from './../components/Button.jsx';
-
+import Member from '../components/Dashboard/MembersDirectory.jsx';
+import Messages from '../components/Dashboard/Messages.jsx';
 function Dashboard() {
   return (
     <>
-      <h1>Start of the project.</h1>
-      <Button>Button</Button>
-      <Button variation="secondary" size="large">
-        Large Button
-      </Button>
+      <div className="container mx-auto">
+        <div className="flex flex-nowrap">
+          <div className="basis-1/4 border-2 border-rose-600">
+            <Messages></Messages>
+          </div>
+          <div className="basis-6/12">
+            <Member></Member>
+          </div>
+          <div className="basis-1/4">
+            <Member></Member>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
