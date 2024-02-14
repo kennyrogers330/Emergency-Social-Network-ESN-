6abddbea9b4aa08f9d949f71ce470ce2bce6c6fa
yuyunfrancis <francisberi@yahoo.com>
2024-02-14 14:41:07 +0200

    allowed cors to be accepted

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,23 +1,24 @@
 import { useNavigate } from 'react-router-dom';
 import React, { useContext } from 'react';
-import Chat from '../components/dashboard/Chat.jsx';
-import Member from '../components/dashboard/MembersDirectory.jsx';
-import Messages from '../components/dashboard/Messages.jsx';
+import Chat from '../components/Dashboard/Chat.jsx';
+import Member from '../components/Dashboard/MembersDirectory.jsx';
+import Messages from '../components/Dashboard/Messages.jsx';
 import { UserContext } from '../context/UserContext.js';
+import { logout } from '../services/AuthServices.js';
 
 function Dashboard() {
   const { setCurrentUser } = useContext(UserContext);
   const navigate = useNavigate();
 
-  const handleLogout = async () => {
-    try {
-      await logout();
-      setCurrentUser(null);
-      navigate('/login', { replace: true });
-    } catch (e) {
-      console.error('Error during logout:', e);
-    }
-  };
+  // const handleLogout = async () => {
+  //   try {
+  //     await logout();
+  //     setCurrentUser(null);
+  //     navigate('/login', { replace: true });
+  //   } catch (e) {
+  //     console.error('Error during logout:', e);
+  //   }
+  // };
 
   return (
     <div className="flex justify-center px-3 h-full max-h-full">
