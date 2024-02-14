d236bb018317048e090327e09c9f7e0890f5154f
kennyrogers330 <kennyrogers330@gmail.com>
2024-02-14 17:29:01 +0200

    cors resolved

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,18 @@ import swaggerUi from "swagger-ui-express";
 import { swaggerOptions } from "./swagger.js";
 import citizeRouter from "./src/middlewares/citizenRoutes.js";
 import ChatInterface from "./src/middlewares/ChatInterface.js";
+import cors from "cors";
+
 const app = express();
 app.use(express.json());
+
+app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  }),
+);
 
 // Swagger setup
 const specs = swaggerJsdoc(swaggerOptions);
