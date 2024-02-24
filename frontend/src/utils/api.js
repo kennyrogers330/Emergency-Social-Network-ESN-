c02201ff72c7cb0266af31da404a14dde0c93d8c
Innocent <twagirimana.innocent@pivotaccess.com>
2024-02-24 11:49:54 +0200

    add api url to backend on render cloud

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1/',
+  baseURL: 'https://fse-rw-s24-rw1-backend.onrender.com/api/v1/',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
