469371210bdca93074a4f90f0ac3d59556b6f7ad
Innocent <twagirimana.innocent@pivotaccess.com>
2024-02-24 13:18:13 +0200

    rm api baseurl env

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const apiUrl = import.meta.env.API_URL;
 
 const api = axios.create({
-  baseURL: `https://fse-rw-s24-rw1-backend.onrender.com/api/v1/`,
+  baseURL: "https://fse-rw-s24-rw1-backend.onrender.com/api/v1/",
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
